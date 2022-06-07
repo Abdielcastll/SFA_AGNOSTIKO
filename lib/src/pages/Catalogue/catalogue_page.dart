00bebd0b624eee3d48941f532b380a7839a6787e
Johnny <johnny130205@gmail.com>
2022-06-07 19:40:56 -0400

    cambiar appbar

--- lib/src/pages/Catalogue/catalogue_page.dart
+++ lib/src/pages/Catalogue/catalogue_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:pwa_sales2go_flutter/src/pages/Catalogue/catalogue_tab.dart';
 import 'package:pwa_sales2go_flutter/src/pages/Catalogue/promotions_tab.dart';
-import 'package:pwa_sales2go_flutter/src/widgets/appbar/appbar_catalogue.dart';
-import 'package:pwa_sales2go_flutter/src/widgets/drawer_widget.dart';
+import 'package:pwa_sales2go_flutter/src/widgets/appbar/appbar_home.dart';
+import 'package:pwa_sales2go_flutter/src/widgets/drawer/drawer_widget.dart';
 
 class CataloguePage extends StatefulWidget {
   const CataloguePage({Key? key}) : super(key: key);
@@ -18,8 +18,9 @@ class _CataloguePageState extends State<CataloguePage> {
       length: 2,
       child: Scaffold(
         drawer: const DrawerWidget(),
-        appBar: const AppBarCatalogue(
+        appBar: const AppBarHome(
           title: 'Catalogo',
+          backgroundColor: Color(0xFF4f42ed),
         ),
         body: Container(
           // decoration: BoxDecoration(),
