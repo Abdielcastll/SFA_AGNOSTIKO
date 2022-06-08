53a3c082b480a0fa18098c840eadca275722d774
Johnny <johnny130205@gmail.com>
2022-06-08 18:35:18 -0400

    Agregar estilos al custom fieldtext

--- lib/src/widgets/custom_text_field/custom_text_field_widget.dart
+++ lib/src/widgets/custom_text_field/custom_text_field_widget.dart
@@ -4,14 +4,14 @@ import 'package:email_validator/email_validator.dart';
 class CustomTextField extends StatefulWidget {
   CustomTextField({
     Key? key,
-    this.iconData,
+    // this.iconData,
     this.textEditingController,
     this.hintText,
     this.isObsecure,
     this.enabled,
   }) : super(key: key);
 
-  IconData? iconData;
+  // IconData? iconData;
   TextEditingController? textEditingController;
   String? hintText;
   bool? isObsecure = true;
@@ -25,42 +25,44 @@ class _CustomTextFieldState extends State<CustomTextField> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: 500.0,
+      height: 50.0,
       decoration: const BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black12,
-            blurRadius: 10,
-            offset: Offset(0, 10),
-          ),
-        ],
+        borderRadius: BorderRadius.all(
+          Radius.circular(50),
+        ),
       ),
-      padding: const EdgeInsets.all(8.0),
-      margin: const EdgeInsets.all(10.0),
+      // padding: const EdgeInsets.all(8.0),
+      margin: const EdgeInsets.only(right: 20.0, left: 20.0, top: 10.0),
       child: TextFormField(
+        maxLines: 1,
         textInputAction: TextInputAction.next,
         enabled: widget.enabled,
+        style: const TextStyle(fontSize: 14.0),
         controller: widget.textEditingController,
         obscureText: widget.isObsecure!,
         cursorColor: Theme.of(context).primaryColor,
         decoration: InputDecoration(
-          border: InputBorder.none,
-          prefixIcon: Container(
-            padding: const EdgeInsets.only(bottom: 8.0),
-            child: Icon(
-              widget.iconData,
-              color: Theme.of(context).primaryColor,
-              size: 25.0,
+          contentPadding:
+              const EdgeInsets.only(left: 20.0, top: 10.0, right: 10.0),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25.0),
+            borderSide: const BorderSide(
+              color: Color(0xFF4f42ed),
             ),
           ),
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Color.fromARGB(255, 191, 191, 191)),
+            borderRadius: BorderRadius.all(Radius.circular(50)),
+          ),
           focusColor: Theme.of(context).primaryColor,
-          hintText: widget.hintText,
+          labelText: widget.hintText,
+          labelStyle: const TextStyle(
+            color: Colors.grey,
+            fontSize: 14.0,
+          ),
         ),
-        // autovalidateMode: AutovalidateMode.onUserInteraction,
-        // validator: (email) => email != null && !EmailValidator.validate(email)
-        //     ? 'Email invalido'
-        //     : null,
       ),
     );
   }
