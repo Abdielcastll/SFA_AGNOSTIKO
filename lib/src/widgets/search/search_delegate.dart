cb58bdb63d2b5da0922e0344c2d3fdea05668053
Johnny <johnny130205@gmail.com>
2022-06-08 18:17:50 -0400

    Crear funciones de search, leading, actions y resultados

---
+++ lib/src/widgets/search/search_delegate.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class DataSearch extends SearchDelegate<String> {
+  String? select = '';
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    // Icono a la izquierda del appbar
+    return IconButton(
+        icon: AnimatedIcon(
+            icon: AnimatedIcons.menu_arrow, progress: transitionAnimation),
+        onPressed: () {
+          print('Clicked leading');
+          Navigator.pop(context);
+        });
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // Crea los resultados a mostrar
+    return Center(
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        color: Colors.blueAccent,
+        child: Text(select!),
+      ),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // Sugerencias cuando el usuario escribe
+    if (query.isEmpty) {
+      return Container();
+    }
+    return Container(
+      child: Column(
+        children: [
+          Text('Sugerencias para: $query'),
+          Text('Sugerencias para: $query'),
+          Text('Sugerencias para: $query'),
+        ],
+      ),
+    );
+  }
+}
