ee32594cf6ea5572eb18fa9b3af9b65052296c44
Johnny <johnny130205@gmail.com>
2022-06-16 17:44:26 -0400

    Modificar tab de productos

--- lib/src/pages/products/products_page.dart
+++ lib/src/pages/products/products_page.dart
@@ -4,7 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:pwa_sales2go_flutter/src/widgets/appbar/appbar_home.dart';
 import 'package:pwa_sales2go_flutter/src/widgets/appbar/bottom_decoration.dart';
-import 'package:pwa_sales2go_flutter/src/widgets/drawer/drawer_widget.dart';
 
 class ProductsPage extends StatefulWidget {
   const ProductsPage({Key? key}) : super(key: key);
@@ -16,70 +15,28 @@ class ProductsPage extends StatefulWidget {
 class _ProductsPageState extends State<ProductsPage> {
   @override
   Widget build(BuildContext context) {
-    final Stream<QuerySnapshot> productsStream =
-        FirebaseFirestore.instance.collection('productos').snapshots();
-    // final Stream<QuerySnapshot> preciosStream =
-    //     FirebaseFirestore.instance.collection('lista_de_precios').snapshots();
-    // final Stream<QuerySnapshot> stockStream =
-    //     FirebaseFirestore.instance.collection('stock').snapshots();
-
     return Scaffold(
-      drawer: const DrawerWidget(),
       appBar: AppBarHome(
         title: 'Productos',
         backgroundColor: Color(0xFF4f42ed),
       ),
       backgroundColor: Colors.white,
       bottomNavigationBar: const BottomDecoration(),
-      body: productsBody(productsStream),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                _productsBody(),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 
-  Widget productsBody(productsStream) {
-    return SingleChildScrollView(
-      child: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        color: Colors.grey[200],
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(height: 15.0),
-              Container(
-                padding: EdgeInsets.only(top: 10.0),
-                child: SingleChildScrollView(
-                  child: StreamBuilder(
-                    stream: productsStream,
-                    builder: (BuildContext context,
-                        AsyncSnapshot<QuerySnapshot> snapshot) {
-                      if (snapshot.hasError) {
-                        return Column(
-                          children: [
-                            Text('Error: ${snapshot.error}'),
-                            SizedBox(height: 10),
-                            Text('Revisar reglas de Firebase'),
-                          ],
-                        );
-                      }
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return Container(
-                          padding: const EdgeInsets.only(top: 60),
-                          child: CircularProgressIndicator(
-                            valueColor: AlwaysStoppedAnimation<Color>(
-                                Color(0xFF4f42ed)),
-                          ),
-                        );
-                      }
-                      return Container(
-                          child: Text('Datos obtenidos con exito'));
-                    },
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+  Widget _productsBody() {
+    return SingleChildScrollView();
   }
 }
