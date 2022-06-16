8ef1b2c93fb5812ec44ec8c11d61dbe786b1a0bd
Johnny <johnny130205@gmail.com>
2022-06-16 17:43:59 -0400

    Crear rutas adicionales

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,28 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:firebase_core/firebase_core.dart';
-
+import 'package:pwa_sales2go_flutter/src/pages/navigation_pages/catalogue/catalogue_page.dart';
 import 'firebase_options.dart';
-//Flutter
+import 'package:pwa_sales2go_flutter/src/pages/auth_pages/login/forgot_password_page.dart';
+import 'package:pwa_sales2go_flutter/src/pages/auth_pages/login/login_page.dart';
+import 'package:pwa_sales2go_flutter/src/pages/auth_pages/splashscreen/splashscreen.dart';
+import 'package:pwa_sales2go_flutter/src/pages/navigation_pages/cart/cart_page.dart';
+import 'package:pwa_sales2go_flutter/src/pages/navigation_pages/cart/checkout_page.dart';
+import 'package:pwa_sales2go_flutter/src/pages/navigation_pages/clients/clients_page.dart';
+import 'package:pwa_sales2go_flutter/src/pages/navigation_pages/dashboard/dashboard_page.dart';
+import 'package:pwa_sales2go_flutter/src/pages/navigation_pages/records/invoices/invoices_page.dart';
+import 'package:pwa_sales2go_flutter/src/pages/navigation_pages/records/orders/orders_page.dart';
+import 'package:pwa_sales2go_flutter/src/pages/navigation_pages/records/records_tabs.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:pwa_sales2go_flutter/src/global/global.dart';
-//Routes
-import 'package:pwa_sales2go_flutter/src/pages/Catalogue/catalogue_page.dart';
-import 'package:pwa_sales2go_flutter/src/widgets/splashscreen/splashscreen_widget.dart';
-import 'package:pwa_sales2go_flutter/src/pages/login/login_page.dart';
-import 'package:pwa_sales2go_flutter/src/pages/login/forgot_password_page.dart';
+import 'package:pwa_sales2go_flutter/src/theme/theme.dart';
+
+import 'package:pwa_sales2go_flutter/src/pages/navigation_pages/navi_pages.dart';
+
 import 'package:pwa_sales2go_flutter/src/pages/products/products_page.dart';
-import 'package:pwa_sales2go_flutter/src/pages/clients/clients_page.dart';
-import 'package:pwa_sales2go_flutter/src/pages/cart/cart_page.dart';
-import 'package:pwa_sales2go_flutter/src/pages/cart/checkout_page.dart';
-import 'package:pwa_sales2go_flutter/src/pages/dashboard/dashboard_page.dart';
-import 'package:pwa_sales2go_flutter/src/pages/invoices/invoices_page.dart';
+
 import 'package:pwa_sales2go_flutter/src/pages/notifications/notifications_page.dart';
-import 'package:pwa_sales2go_flutter/src/pages/orders/orders_page.dart';
 import 'package:pwa_sales2go_flutter/src/pages/visits/visits_page.dart';
 
 Future<void> main() async {
@@ -42,28 +45,24 @@ class SfaAgnostiko extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'SFA Agnostiko',
-      theme: ThemeData(
-        primaryColor: const Color(0xFF4f42ed),
-      ),
+      theme: myTheme,
       initialRoute: 'splashscreen',
       routes: {
-        //
         'splashscreen': (BuildContext context) => SplashScreenWidget(),
+        'tabs': (BuildContext context) => NaviPages(),
         'dashboard': (BuildContext context) => DashboardPage(),
-        //
         'login': (BuildContext context) => LoginPage(),
         'password_reset': (BuildContext context) => ForgotPasswordPage(),
-        //
         'cart': (BuildContext context) => CartPage(),
         'checkout': (BuildContext context) => CheckoutPage(),
         'notifications': (BuildContext context) => NotificationsPage(),
-        //
         'catalogue': (BuildContext context) => CataloguePage(),
         'products': (BuildContext context) => ProductsPage(),
         'clients': (BuildContext context) => ClientsPage(),
         'orders': (BuildContext context) => OrdersPage(),
         'invoices': (BuildContext context) => InvoicesPage(),
         'visits': (BuildContext context) => VisitsPage(),
+        'records': (BuildContext context) => RecordTabs(),
       },
     );
   }
