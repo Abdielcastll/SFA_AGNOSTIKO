12127143b51c10b7a28a3186738991db119401b5
Johnny <johnny130205@gmail.com>
2022-06-16 17:40:39 -0400

    crear tema color principal y secundario de la aplicacion

---
+++ lib/src/theme/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+final myTheme = ThemeData.light().copyWith(
+  primaryColor: const Color(0xFF4f42ed),
+  colorScheme: const ColorScheme.light(
+    secondary: Color(0xFF4f42ed),
+    tertiary: Color(0xFF2f278e),
+  ),
+);
