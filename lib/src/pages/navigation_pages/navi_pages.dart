a4fc28089d4d63fa7f68042fb3002b7bd377c2ec
Johnny <johnny130205@gmail.com>
2022-06-17 17:24:10 -0400

    Modificar bottom bar navegation para que tenga un decorador permanente estilizado

--- lib/src/pages/navigation_pages/navi_pages.dart
+++ lib/src/pages/navigation_pages/navi_pages.dart
@@ -8,6 +8,7 @@ import 'package:pwa_sales2go_flutter/src/pages/navigation_pages/dashboard/dashbo
 import 'package:pwa_sales2go_flutter/src/pages/navigation_pages/records/records_tabs.dart';
 import 'package:pwa_sales2go_flutter/src/theme/theme.dart';
 import 'package:pwa_sales2go_flutter/src/widgets/appbar/appbar_home.dart';
+import 'package:pwa_sales2go_flutter/src/widgets/appbar/bottom_decoration.dart';
 
 class NaviPages extends StatefulWidget {
   const NaviPages({Key? key}) : super(key: key);
@@ -33,71 +34,82 @@ class _NaviPagesState extends State<NaviPages> {
         title: 'SFA Agnostiko',
       ),
       body: screens[index],
-      bottomNavigationBar: NavigationBarTheme(
-        data: NavigationBarThemeData(
-          labelTextStyle: MaterialStateProperty.all(
-            TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w500,
-              color: Colors.white,
+      bottomNavigationBar: Stack(
+        // mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          NavigationBarTheme(
+            data: NavigationBarThemeData(
+              indicatorColor: Colors.white.withOpacity(0.2),
+              labelTextStyle: MaterialStateProperty.all(
+                TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            child: NavigationBar(
+              height: 56.0,
+              backgroundColor: myTheme.colorScheme.secondary,
+              labelBehavior:
+                  NavigationDestinationLabelBehavior.onlyShowSelected,
+              animationDuration: Duration(seconds: 2),
+              selectedIndex: index,
+              onDestinationSelected: (int i) {
+                setState(() => index = i);
+              },
+              // ignore: prefer_const_literals_to_create_immutables
+              destinations: [
+                NavigationDestination(
+                  icon: Icon(Icons.dashboard_outlined,
+                      color: myTheme.colorScheme.tertiary),
+                  selectedIcon: Icon(
+                    Icons.dashboard_rounded,
+                    color: Colors.white,
+                  ),
+                  label: 'Inicio',
+                ),
+                NavigationDestination(
+                  icon: Icon(
+                    Icons.storefront_outlined,
+                    color: myTheme.colorScheme.tertiary,
+                  ),
+                  selectedIcon: Icon(
+                    Icons.storefront_rounded,
+                    color: Colors.white,
+                  ),
+                  label: 'Clientes',
+                ),
+                NavigationDestination(
+                  icon: Icon(
+                    Icons.shopping_bag_outlined,
+                    color: myTheme.colorScheme.tertiary,
+                  ),
+                  selectedIcon: Icon(
+                    Icons.shopping_bag_rounded,
+                    color: Colors.white,
+                  ),
+                  label: 'Catalogo',
+                ),
+                NavigationDestination(
+                  icon: Icon(
+                    Icons.shopping_cart_outlined,
+                    color: myTheme.colorScheme.tertiary,
+                  ),
+                  selectedIcon: Icon(
+                    Icons.shopping_cart_rounded,
+                    color: Colors.white,
+                  ),
+                  label: 'Carrito',
+                ),
+              ],
             ),
           ),
-        ),
-        child: NavigationBar(
-          height: 56.0,
-          backgroundColor: myTheme.colorScheme.secondary,
-          labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-          animationDuration: Duration(seconds: 2),
-          selectedIndex: index,
-          onDestinationSelected: (int i) {
-            setState(() => index = i);
-          },
-          // ignore: prefer_const_literals_to_create_immutables
-          destinations: [
-            NavigationDestination(
-              icon: Icon(Icons.dashboard_outlined,
-                  color: myTheme.colorScheme.tertiary),
-              selectedIcon: Icon(
-                Icons.dashboard_rounded,
-                color: Colors.white,
-              ),
-              label: 'Inicio',
-            ),
-            NavigationDestination(
-              icon: Icon(
-                Icons.storefront_outlined,
-                color: myTheme.colorScheme.tertiary,
-              ),
-              selectedIcon: Icon(
-                Icons.storefront_rounded,
-                color: Colors.white,
-              ),
-              label: 'Clientes',
-            ),
-            NavigationDestination(
-              icon: Icon(
-                Icons.shopping_bag_outlined,
-                color: myTheme.colorScheme.tertiary,
-              ),
-              selectedIcon: Icon(
-                Icons.shopping_bag_rounded,
-                color: Colors.white,
-              ),
-              label: 'Catalogo',
-            ),
-            NavigationDestination(
-              icon: Icon(
-                Icons.shopping_cart_outlined,
-                color: myTheme.colorScheme.tertiary,
-              ),
-              selectedIcon: Icon(
-                Icons.shopping_cart_rounded,
-                color: Colors.white,
-              ),
-              label: 'Carrito',
-            ),
-          ],
-        ),
+          Container(
+            padding: const EdgeInsets.only(top: 56.0),
+            child: BottomDecoration(),
+          ),
+        ],
       ),
     );
   }
