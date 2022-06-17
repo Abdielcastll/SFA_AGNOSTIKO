7156bcb35caa6b1f2a49d239647e6eb15a0d53ff
Johnny <johnny130205@gmail.com>
2022-06-17 17:24:52 -0400

    Modificar para ser decorador permanente

--- lib/src/widgets/appbar/bottom_decoration.dart
+++ lib/src/widgets/appbar/bottom_decoration.dart
@@ -8,18 +8,19 @@ class BottomDecoration extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 30.0,
+      height: 20.0,
       child: BottomAppBar(
         color: const Color(0xFF4f42ed),
         child: Center(
-            child: Container(
-          padding: const EdgeInsets.only(top: 10.0),
-          width: 80.0,
-          child: const Divider(
-            color: Colors.white,
-            thickness: 2,
+          child: Container(
+            padding: const EdgeInsets.only(top: 3.0),
+            width: 80.0,
+            child: const Divider(
+              color: Colors.white,
+              thickness: 2,
+            ),
           ),
-        )),
+        ),
       ),
     );
   }
