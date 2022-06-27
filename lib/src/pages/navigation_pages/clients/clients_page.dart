29c5968a2856327e93e536072724b9fa0ef24133
Johnny <johnny130205@gmail.com>
2022-06-27 17:39:35 -0400

    Crear display de data con firestore de clientes

--- lib/src/pages/navigation_pages/clients/clients_page.dart
+++ lib/src/pages/navigation_pages/clients/clients_page.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:pwa_sales2go_flutter/src/pages/navigation_pages/clients/client_details.dart';
 
 class ClientsPage extends StatelessWidget {
   const ClientsPage({Key? key}) : super(key: key);
@@ -35,6 +37,8 @@ class _ClientList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _clientsQuery = FirebaseFirestore.instance.collection('clientes');
+
     final List<Map<String, dynamic>> _clients = [
       {
         'name': 'Cliente 1',
@@ -85,28 +89,45 @@ class _ClientList extends StatelessWidget {
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height * 0.75,
       color: Colors.grey[100],
-      child: ListView.builder(
-        physics: BouncingScrollPhysics(),
-        scrollDirection: Axis.vertical,
-        itemCount: _clients.length,
-        itemBuilder: (BuildContext context, int i) {
-          final cProfile = _clients[i];
-          return Padding(
-            padding: EdgeInsets.symmetric(horizontal: 5.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              // ignore: prefer_const_literals_to_create_immutables
-              children: [
-                _ClientCard(
-                  name: cProfile['name'],
-                  phone: cProfile['phone'],
-                  email: cProfile['email'],
-                  rubro: cProfile['rubro'],
+      child: StreamBuilder<QuerySnapshot>(
+        stream: _clientsQuery.orderBy('nombre').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.connectionState == ConnectionState.active) {
+            if (snapshot.data!.docs.isNotEmpty) {
+              return Scrollbar(
+                child: ListView.builder(
+                  physics: BouncingScrollPhysics(),
+                  scrollDirection: Axis.vertical,
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final clients = snapshot.data!.docs[index];
+                    return Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 5.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        // ignore: prefer_const_literals_to_create_immutables
+                        children: [
+                          _ClientCard(
+                            name: clients['nombre'],
+                            phone: clients['telefono'],
+                            email: clients['email'],
+                            active: clients['activo'],
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 ),
-              ],
-            ),
-          );
+              );
+            } else {
+              Center(child: Text('No hay clientes disponibles'));
+            }
+          }
+          return Center(
+              child: Text('Hubo un problema conectandose a los clientes'));
         },
       ),
     );
@@ -119,13 +140,13 @@ class _ClientCard extends StatelessWidget {
     required this.name,
     required this.phone,
     required this.email,
-    required this.rubro,
+    required this.active,
   }) : super(key: key);
 
   final String name;
   final String phone;
   final String email;
-  final String rubro;
+  final bool active;
 
   @override
   Widget build(BuildContext context) {
@@ -134,6 +155,12 @@ class _ClientCard extends StatelessWidget {
       child: GestureDetector(
         onTap: () {
           print('tappeada tarjeta para acceder al cliente');
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => ClientDetails(),
+            ),
+          );
         },
         child: Container(
           width: 360.0,
@@ -143,7 +170,7 @@ class _ClientCard extends StatelessWidget {
             borderRadius: BorderRadius.circular(10.0),
           ),
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               Container(
                 padding: EdgeInsets.only(left: 10.0, top: 20.0),
@@ -151,19 +178,30 @@ class _ClientCard extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      name,
-                      style: TextStyle(
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.bold,
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.35,
+                      child: Text(
+                        name,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                     SizedBox(height: 5.0),
-                    Text(
-                      email,
-                      style: TextStyle(
-                        fontSize: 12.0,
-                        color: Colors.grey[500]?.withOpacity(0.8),
+                    SizedBox(
+                      width: 150,
+                      child: Text(
+                        email,
+                        style: TextStyle(
+                          fontSize: 12.0,
+                          color: Colors.grey[500]?.withOpacity(0.8),
+                        ),
+                        textAlign: TextAlign.start,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
                       ),
                     ),
                   ],
@@ -171,16 +209,22 @@ class _ClientCard extends StatelessWidget {
               ),
               SizedBox(width: 10.0),
               Padding(
-                padding: EdgeInsets.only(top: 25.0),
-                child: Text(
-                  phone,
-                  style: TextStyle(
-                    fontSize: 12.0,
-                    color: Colors.grey[500]?.withOpacity(0.8),
+                padding: EdgeInsets.only(top: 27),
+                child: SizedBox(
+                  width: 120,
+                  height: 50,
+                  child: Text(
+                    phone,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontSize: 12.0,
+                      color: Colors.grey[500]?.withOpacity(0.8),
+                    ),
                   ),
                 ),
               ),
-              SizedBox(width: 30.0),
+              SizedBox(width: 10.0),
               Column(
                 children: [
                   Padding(
@@ -188,11 +232,22 @@ class _ClientCard extends StatelessWidget {
                     child: Icon(Icons.arrow_forward_ios),
                   ),
                   SizedBox(height: 15.0),
-                  Text(
-                    rubro,
-                    style: TextStyle(
-                      fontSize: 12.0,
-                      color: Colors.grey[500]?.withOpacity(0.8),
+                  Container(
+                    margin: EdgeInsets.only(
+                      right: 10,
+                    ),
+                    child: Text(
+                      active ? 'Activo' : 'Inactivo',
+                      style: active
+                          ? TextStyle(
+                              fontSize: 12.0,
+                              color: Colors.green[500]?.withOpacity(0.8))
+                          : TextStyle(
+                              fontSize: 12.0,
+                              color: Colors.red[500]?.withOpacity(0.8),
+                            ),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
                     ),
                   ),
                 ],
