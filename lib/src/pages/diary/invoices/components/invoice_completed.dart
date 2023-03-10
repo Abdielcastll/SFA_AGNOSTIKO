0f12f4bf26448fc7f287fce096d2071dfb3c7ad8
HrHrM <johnny130205@gmail.com>
2023-03-10 13:28:50 -0400

    Modificar Mediaquerys de las pantallas para terminal

--- lib/src/pages/diary/invoices/components/invoice_completed.dart
+++ lib/src/pages/diary/invoices/components/invoice_completed.dart
@@ -109,7 +109,7 @@ class _InvoicesListState extends State<InvoicesList> {
 
                         DateTime? newDate = await showDatePicker(
                           context: context,
-                          initialDate: currentDay.toDate(),
+                          initialDate: DateTime.now(),
                           firstDate: DateTime(2010),
                           lastDate: DateTime(2500),
                         );
@@ -166,7 +166,7 @@ class _InvoicesListState extends State<InvoicesList> {
         invoicesList.isNotEmpty
             ? SingleChildScrollView(
                 child: Container(
-                  height: MediaQuery.of(context).size.height * 0.65,
+                  height: MediaQuery.of(context).size.height * 0.52,
                   child: Scrollbar(
                     child: ListView.builder(
                       physics: ClampingScrollPhysics(),
