4ff4ece53bc88e28c0582f0a84237fe3d2b79509
Armando Hernandez <avargas@solopago.com>
2024-05-08 15:02:05 -0600

    color adjust

--- lib/dialogs/go_to_pinpad_dialog.dart
+++ lib/dialogs/go_to_pinpad_dialog.dart
@@ -17,7 +17,7 @@ void showGoToPinpadDialog(BuildContext context, String message,
     Navigator.of(context).pop();
   });
   showDialog(
-    barrierColor: Color.fromARGB(197, 255, 255, 255),
+    barrierColor: Color.fromARGB(203, 212, 212, 212),
     context: context,
     barrierDismissible: false,
     builder: (BuildContext context) {
