7f5074933c79fa7c3be06582602a43efeb769708
Armando Hernandez <avargas@solopago.com>
2024-12-04 16:07:29 -0600

    images from firebase to cache

--- lib/src/global/logo_widget_dynamic.dart
+++ lib/src/global/logo_widget_dynamic.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:pwa_sales2go_flutter/src/provider/remote_config_provider.dart';
 import 'package:pwa_sales2go_flutter/src/utils/multitenant-config.dart';
 
@@ -37,6 +40,17 @@ class _LogoFromFirebaseState extends State<LogoFromFirebase> {
     }
   }
 
+  Future<File?> fetchLogoWithCache(String imageUrl) async {
+    try {
+      // Use DefaultCacheManager to fetch and cache the image
+      final cacheManager = DefaultCacheManager();
+      return await cacheManager.getSingleFile(imageUrl);
+    } catch (e) {
+      print('Error fetching logo with cache: $e');
+      return null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<String?>(
@@ -47,9 +61,22 @@ class _LogoFromFirebaseState extends State<LogoFromFirebase> {
         } else if (snapshot.hasError) {
           return const Text('Error loading logo'); // Handle errors gracefully
         } else if (snapshot.hasData && snapshot.data != null) {
-          return Image.network(
-            snapshot.data!,
-            fit: BoxFit.contain, // Adjust the fit as needed
+          return FutureBuilder<File?>(
+            future: fetchLogoWithCache(snapshot.data!),
+            builder: (context, cacheSnapshot) {
+              if (cacheSnapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator(); // Show loading while fetching from cache
+              } else if (cacheSnapshot.hasError) {
+                return const Text('Error loading cached logo');
+              } else if (cacheSnapshot.hasData && cacheSnapshot.data != null) {
+                return Image.file(
+                  cacheSnapshot.data!,
+                  fit: BoxFit.contain, // Adjust the fit as needed
+                );
+              } else {
+                return const Text('No cached logo available');
+              }
+            },
           );
         } else {
           return const Text('No logo available'); // Handle null data
--- lib/src/widgets/invoices_alerts_and_dialogs/onTapPayment.dart
+++ lib/src/widgets/invoices_alerts_and_dialogs/onTapPayment.dart
@@ -2,6 +2,7 @@
 
 import 'dart:async';
 import 'dart:ui' as ui;
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:agnostiko/agnostiko.dart';
@@ -231,21 +232,23 @@ Future refund(BuildContext context, payment, String pleaseWait, Client client,
 
 Future<ui.Image?> networkImageToUiImage(String imageUrl) async {
   try {
-    // Fetch the image data from the network
-    final http.Response response = await http.get(Uri.parse(imageUrl));
+    // Use the default cache manager to fetch the image file
+    final cacheManager = DefaultCacheManager();
+    final file = await cacheManager.getSingleFile(imageUrl);
 
-    if (response.statusCode == 200) {
+    if (file != null) {
+      // Read the file as bytes
+      final Uint8List imageData = await file.readAsBytes();
+
       // Convert the raw bytes into a ui.Image
-      Uint8List imageData = response.bodyBytes;
       final Completer<ui.Image> completer = Completer();
-
       ui.decodeImageFromList(imageData, (ui.Image img) {
         completer.complete(img);
       });
 
       return completer.future;
     } else {
-      print('Failed to load image. Status code: ${response.statusCode}');
+      print('Failed to load image from cache.');
       return null;
     }
   } catch (e) {
