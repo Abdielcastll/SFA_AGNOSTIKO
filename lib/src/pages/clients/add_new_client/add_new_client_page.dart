3a8ae7d34340a4d146e1cd9dfe8309b66cafe5c3
Armando Hernandez <avargas@solopago.com>
2025-01-03 09:48:09 -0600

    remove dead code

--- lib/src/pages/clients/add_new_client/add_new_client_page.dart
+++ lib/src/pages/clients/add_new_client/add_new_client_page.dart
@@ -98,27 +98,6 @@ class _AddClientPageBodyState extends State<AddClientPageBody> {
   late String? latitude = '';
   late String? longitude = '';
 
-  // Future<Position> _getCurrentLocation() async {
-  //   bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  //   if (!serviceEnabled) {
-  //     return Future.error('Location services are disabled');
-  //   }
-  //   LocationPermission permission = await Geolocator.checkPermission();
-  //   if (permission == LocationPermission.denied) {
-  //     permission = await Geolocator.requestPermission();
-  //     if (permission == LocationPermission.denied) {
-  //       return Future.error('Location permissions are denied');
-  //     }
-  //   }
-  //   if (permission == LocationPermission.deniedForever) {
-  //     return Future.error(
-  //         'Location permissions are permanently denied, we cannot request permission');
-  //   }
-  //   return await Geolocator.getCurrentPosition(
-  //     forceAndroidLocationManager: true,
-  //   );
-  // }
-
   File? imageFile;
   @override
   void initState() {
