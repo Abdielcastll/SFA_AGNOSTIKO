0ce738351ba6c581150ccf54c1fd7a5ae6e1dfb2
Armando Hernandez <avargas@solopago.com>
2025-02-21 17:09:45 -0600

    1.7.4

--- lib/src/pages/card_input/card_input.dart
+++ lib/src/pages/card_input/card_input.dart
@@ -308,10 +308,14 @@ class _CardInputViewState extends State<CardInputView> {
               paymentBody!.client, paymentBody!.invoiceNumber);
           Navigator.pop(context);
           Navigator.pop(context);
+          return;
         } else {
           Navigator.popUntil(context, (route) => route.isFirst == true);
+          return;
         }
-      } else if (globalRemoteConfig.onlyFullPaymentWithCard!) {
+      }
+
+      if (globalRemoteConfig.onlyFullPaymentWithCard!) {
         await cancelPaymentProcess(
             paymentBody!.client, paymentBody!.invoiceNumber);
         Navigator.pop(context);
