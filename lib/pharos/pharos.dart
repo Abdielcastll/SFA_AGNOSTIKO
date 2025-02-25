ecee6ea601709e708c5ee5d4b6f082b522dc7d24
eruiz00-solopago <eruiz@solopago.com>
2025-02-25 10:15:32 -0600

    feat<transaction info> Soluciona el error del stan nulo en pantalla de resumen de transacción y ticket

--- lib/pharos/pharos.dart
+++ lib/pharos/pharos.dart
@@ -66,8 +66,9 @@ Future<Map<String, dynamic>> pharosGenerateSaleMsg(
   if (transactionArgs.entryMode == EntryMode.Contact ||
       transactionArgs.entryMode == EntryMode.Contactless ||
       transactionArgs.entryMode == EntryMode.Magstripe) {
-    final stan = await getSTANCounterAndIncrement();
-    transactionArgs.stan = stan;
+        // Aquí esta el stan, solo hay que asignarlo antes de llegar aquí
+    // final stan = await getSTANCounterAndIncrement();
+    // transactionArgs.stan = stan;
 
     String track2;
     String ksn;
@@ -92,7 +93,7 @@ Future<Map<String, dynamic>> pharosGenerateSaleMsg(
       tags: tags,
     );
     return PharosCardSaleRequest(
-      stan: stan.toString(),
+      stan: transactionArgs.stan.toString(),
       date: dateStr,
       card: card,
       amount: amount,
--- lib/src/pages/card_input/card_input.dart
+++ lib/src/pages/card_input/card_input.dart
@@ -377,6 +377,9 @@ class _CardInputViewState extends State<CardInputView> {
     _pinProcessFlag = false;
     final transactionStream = startEmvTransaction(params);
     transactionArgs?.emvStream = transactionStream;
+    // ? Si ya se está ejecutando una transacción, se debe asignar el STAN
+    transactionArgs?.stan = await getSTANCounterAndIncrement();
+    debugPrint("STAN de la transacción ${transactionArgs?.stan}");
 
     try {
       await for (final event in transactionStream) {
